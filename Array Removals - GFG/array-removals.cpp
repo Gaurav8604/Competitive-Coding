593d3b5441647d63b40f519186f9cf8674ae70ff
Gaurav-8604 <106819107+Gaurav-8604@users.noreply.github.com>
2022-11-03 16:45:41 +0530

    Added solution - LeetHub

---
+++ Array Removals - GFG/array-removals.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int removals(vector<int>& arr, int k){
+        int s=0,end=0;
+        sort(arr.begin(),arr.end());
+        int len=-1;
+        while(end<arr.size())
+        {
+            if(arr[end]-arr[start]<=k)
+            {
+                len=max(len,end-start+1);
+                end++;
+            }
+            else start++;
+        }
+        return arr.size()-len;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        Solution ob;
+        int ans = ob.removals(arr,k);
+        
+        cout<<ans<<endl;
+    }
+}
+
+
+
+// } Driver Code Ends
