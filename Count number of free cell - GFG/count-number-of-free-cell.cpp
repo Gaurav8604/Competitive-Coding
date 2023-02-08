a58a0a1d53383734340c020e4a551df096e2da6d
Gaurav Patil <106819107+Gaurav-8604@users.noreply.github.com>
2023-02-08 11:30:06 +0530

    Added solution - LeetHub

--- Count number of free cell - GFG/count-number-of-free-cell.cpp
+++ Count number of free cell - GFG/count-number-of-free-cell.cpp
@@ -12,52 +12,22 @@ using namespace std;
 
 class Solution{
   public:
-//   void rowcolassign(int n,int r,int c,vector<vector<int>> &mat)
-//   {
-//       for(int i=0;i<n;i++)
-//         mat[r-1][i]=1;
-//       for(int i=0;i<n;i++)
-//         mat[i][c-1]=1;
-//   }
-//   int zerocount(vector<vector<int>> mat)
-//   {
-//       int ans=0;
-//       for(int i=0;i<mat.size();i++)
-//       {
-//           for(int j=0;j<mat.size();j++)
-//             if(mat[i][j]==0)
-//                 ans++;
-//       }
-//       return ans;
-//   }
   vector<long long int> countZero(int n, int k, vector<vector<int>>& arr){
-    // vector<int> temp(n,0);
-    // vector<vector<int>> mat(n,temp);
     unordered_set<int> row,col;
     int temp=0;
     vector<long long int> ans(0);
     for(int i=0;i<arr.size();i++)
     {
-        // rowcolassign(n,arr[i][0],arr[i][1],mat);
-        // ans.push_back(zerocount(mat));
         int rows=row.size();
         int cols=col.size();
         row.insert(arr[i][0]);
         col.insert(arr[i][1]);
         if(rows!=row.size() && cols!=col.size())
-        {
             temp+=2*n-cols-rows-1;
-        }
         else if(rows!=row.size())
-        {
             temp+=n-cols;
-        }
         else if(cols!=col.size())
-        {
             temp+=n-rows;
-        }
-        // cout<<cols<<" "<<rows<<endl;
-        // cout<<temp<<endl;
         ans.push_back(pow(n,2)-temp);
         
     }
