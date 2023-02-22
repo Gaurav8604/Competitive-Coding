641dc8ec83ac6852d0d145c3047d71c97839e294
Gaurav Patil <106819107+Gaurav8604@users.noreply.github.com>
2023-02-22 16:27:57 +0530

    Added solution - LeetHub

---
+++ Summed Matrix - GFG/summed-matrix.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        if(q>n+n) return 0;
+        long long temp=n+1;
+        long long temp2=abs(q-temp);
+        long long ans=n-temp2;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
