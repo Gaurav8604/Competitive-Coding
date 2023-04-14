9c6a12c905547748a21879d5e23d4be83725223a
Gaurav Patil <106819107+Gaurav8604@users.noreply.github.com>
2023-04-14 13:56:40 +0530

    Added solution - LeetHub

---
+++ Remove the balls - GFG/remove-the-balls.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int finLength(int N, vector<int> color, vector<int> radius) {
+        int count=0;
+        stack<pair<int,int>> s;
+        for(int i=0;i<N;i++)
+        {
+            pair<int,int> p={color[i],radius[i]};
+            if(!s.empty() && s.top()==p)
+            s.pop();
+            else s.push(p);
+        }
+        return s.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        vector<int> color(n), radius(n);
+        for (int i = 0; i < n; i++) cin >> color[i];
+        for (int i = 0; i < n; i++) cin >> radius[i];
+        int ans = sol.finLength(n, color, radius);
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
